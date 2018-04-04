7358f6893300dee675c5c84cedd955b27791074b
Jacqui Giese <jacqui.giese@gmail.com>
2018-04-03 22:59:58 -0700

    get bootstrap working

--- web-portfolio/src/app/app.module.ts
+++ web-portfolio/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+
 import { AppComponent } from './app.component';
 import { HomepageComponent } from './homepage/homepage.component';
 import { AppRoutingModule } from './/app-routing.module';
